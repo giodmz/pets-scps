fcc9974e1b8a075c26ebd1943af3cd654d5912e1
Giovane Damazio <giovxnedamazio@gmail.com>
2025-08-12 09:21:50 -0300

    Pet DTO

---
+++ src/main/java/com/pets/dto/PetDTO.java
@@ -0,0 +1,33 @@
+package com.pets.dto;
+
+import com.pets.entities.Pet;
+import com.pets.enums.Gender;
+import com.pets.enums.Species;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class PetDTO implements Serializable {
+
+    Integer id;
+    String name;
+    Gender gender;
+    Integer age;
+    Double weight;
+    Species species;
+
+    public PetDTO(Pet obj) {
+        id = obj.getId();
+        name = obj.getName();
+        gender = obj.getGender();
+        age = obj.getAge();
+        weight = obj.getWeight();
+        species = obj.getSpecies();
+    }
+
+}
