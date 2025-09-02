ee640efa12196365b0fe37cb0bc9c9ab72facc40
Giovane Damazio <giovxnedamazio@gmail.com>
2025-09-02 16:07:52 -0300

    adopter service

---
+++ src/main/java/com/pets/resources/AdopterResource.java
@@ -0,0 +1,69 @@
+package com.pets.resources;
+
+
+
+import com.pets.services.AdopterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequestMapping(value="/adopters")
+public class AdopterResource {
+    @Autowired
+    private AdopterService service;
+
+    @GetMapping
+    public ResponseEntity<List<AdopterDTO>> findAll() {
+        List<Adopter> list = service.findAll();
+        // Adopter -> AdopterDTO
+        List<AdopterDTO> listDto = list.stream().map(AdopterDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AdopterDTO> findById(@PathVariable Integer id) {
+        Adopter obj = service.findById(id);
+        return ResponseEntity.ok().body(new AdopterDTO(obj));
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody AdopterDTO objDto, @PathVariable Integer id){
+        Adopter obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> update(@RequestBody AdopterDTO objDto){
+        Adopter obj = service.fromDTO(objDto);
+        obj = service.update(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build(); // 204
+    }
+
+    @GetMapping("/{id}/adopters")
+    public ResponseEntity<List<Adopter>> findAdopter(@PathVariable Integer id){
+        Adopter obj = service.findById(id);
+        return ResponseEntity.ok().body(Collections.singletonList(obj.getAdopter()));
+    }
+}
---
+++ src/main/java/com/pets/services/AdopterService.java
@@ -0,0 +1,75 @@
+package com.pets.services;
+
+
+import com.pets.dto.PetDTO;
+import com.pets.entities.Adopter;
+import com.pets.entities.Pet;
+import com.pets.exceptions.ObjectNotFoundException;
+import com.pets.repository.AdopterRepository;
+import com.pets.repository.PetRepository;
+import com.sun.jdi.ObjectCollectedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdopterService {
+    @Autowired
+    private AdopterRepository rep;
+
+    public List<Adopter> findAll() {
+        return rep.findAll();
+    }
+
+    public Adopter findById(Integer id) {
+        requiredValidId(id);
+        if (id == null || id <= 0) {
+            throw new IllegalArgumentException("Invalid ID: " + id);
+        }
+        return rep.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Adopter not found. (Id: " + id + ")"));
+    }
+
+
+    public void insert(Adopter obj) {
+        rep.save(obj);
+    }
+
+    public void delete(Integer id){
+        requiredValidId(id);
+        rep.findById(id).orElseThrow(ObjectCollectedException::new);
+        rep.deleteById(id);
+    }
+
+    public Adopter update(Adopter obj) {
+        requiredValidId(obj.getId());
+        Adopter newObj = rep.findById(obj.getId()).orElseThrow(ObjectCollectedException::new);
+        updateData(newObj, obj);
+        return rep.save(newObj);
+    }
+
+    private void updateData(Adopter newObj, Adopter obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+        newObj.setContact(obj.getContact());
+        newObj.setAddress(obj.getAddress());
+        newObj.setPets(obj.getPets());
+    }
+
+//    public Adopter fromDTO (AdopterDTO objDto) {
+//        return new Adopter(objDto.getId(),
+//                objDto.getName(),
+//                objDto.getGender(),
+//                objDto.getAge(),
+//                objDto.getWeight(),
+//                objDto.getSpecies(),
+//                objDto.getAdopter());
+//    }
+
+    private void requiredValidId(Integer id){
+        if (id <= 0){
+            throw new IllegalArgumentException("Invalid id");
+        }
+    }
+}
