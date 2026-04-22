9d596e4543daaa304647c34394ab31bad9968d51
Giovane Damazio <giovxnedamazio@gmail.com>
2026-04-21 21:31:38 -0300

    user details service

---
+++ src/main/java/com/pets/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.pets.services;
+
+import com.pets.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository rep;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return rep.findByEmail(email)
+                .map(user -> User.builder()
+                        .username(user.getEmail())
+                        .password(user.getPassword())
+                        .roles("USER")
+                        .build())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+    }
+}
