a9b01b82108a6efdf533692645ebffcad4eb914b
Giovane Damazio <giovxnedamazio@gmail.com>
2026-04-30 10:47:53 -0300

    fix: testes falhando por causa da autenticação do spring security

--- src/main/java/com/pets/controllers/AuthController.java
+++ src/main/java/com/pets/controllers/AuthController.java
@@ -5,6 +5,7 @@ import com.pets.services.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Auth", description = "Authentication and user registration")
 public class AuthController {
 
     private final AuthService service;
--- src/test/java/resources/AdopterControllerTest.java
+++ src/test/java/resources/AdopterControllerTest.java
@@ -6,9 +6,12 @@ import com.pets.controllers.AdopterController;
 import com.pets.dto.AdopterDTO;
 import com.pets.entities.Adopter;
 import com.pets.services.AdopterService;
+import com.pets.services.JwtService;
+import com.pets.services.UserDetailsServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,9 +33,16 @@ public class AdopterControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockitoBean
+    private UserDetailsServiceImpl userDetailsService;
+
+    @MockitoBean
+    private JwtService jwtService;
+
     @Autowired
     private MockMvc mockMvc;
 
+    @WithMockUser
     @Test
     void shouldReturnPetsByName() throws Exception {
         Adopter adopter = new Adopter();
